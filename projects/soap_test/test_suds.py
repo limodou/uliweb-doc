86818ee1367b00a0d57cec4d0c053ee8c4a3b0b3
limodou <limodou@gmail.com>
2012-08-01 20:47:16 +0800

    fix examples' bugs

--- projects/soap_test/test_suds.py
+++ projects/soap_test/test_suds.py
@@ -13,12 +13,12 @@ print client
 
 result = client.service.hello('limodou')
 print 'test1:', result
-#result = client.service.echo('hello')
-#print 'test2:', result
-#result = client.service.add({'int':[1,2]})
-#print 'test3:', result
-#result = client.service.string({'string':[u'中', u'国']})
-#print 'test4:', result
-#result = client.service.Adder(p={'a':10, 'b':20}, c=[{'d':[decimal.Decimal('1.0'),decimal.Decimal('2.0')]}],
-#    dt=date.today())
-#print 'test5:', result
+result = client.service.echo('hello')
+print 'test2:', result
+result = client.service.add({'int':[1,2]})
+print 'test3:', result
+result = client.service.string({'string':[u'中', u'国']})
+print 'test4:', result
+result = client.service.Adder(p={'a':10, 'b':20}, c=[{'d':[decimal.Decimal('1.0'),decimal.Decimal('2.0')]}],
+    dt=date.today())
+print 'test5:', result
