b591afd8568cd7f686b5808847f3e5d81f4d36f2
limodou <limodou@gmail.com>
2013-09-03 14:07:03 +0800

    test refactor of upload

--- projects/upload_demo/apps/test/views.py
+++ projects/upload_demo/apps/test/views.py
@@ -26,3 +26,33 @@ def deletefile():
     if filename:
         functions.delete_filename(filename)
     return redirect(url_for(index))
+
+@expose('/list')
+def list():
+    File = get_model('file')
+    return {'objects':File.all()}
+
+@expose('/upload')
+def upload():
+    File = get_model('file')
+    view = functions.AddView(File, ok_url='/list', upload_to='aaa', upload_to_sub='bbb', fileserving_config='UPLOAD_TEST')
+    return view.run()
+
+@expose('/edit/<id>')
+def edit(id):
+    File = get_model('file')
+    obj = File.get(int(id))
+    view = functions.EditView(File, obj=obj, ok_url='/list', upload_to='aaa', upload_to_sub='bbb', fileserving_config='UPLOAD_TEST')
+    return view.run()
+
+@expose('/delete/<id>')
+def delete(id):
+    File = get_model('file')
+    obj = File.get(int(id))
+
+    def pre_delete(obj):
+        fileserving = functions.get_fileserving('UPLOAD_TEST')
+        fileserving.delete_filename(obj.filename)
+
+    view = functions.DeleteView(File, obj=obj, ok_url='/list', pre_delete=pre_delete)
+    return view.run()
